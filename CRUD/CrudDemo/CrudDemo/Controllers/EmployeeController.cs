5d6f0b168f5dbb8cf03efdfcfffa5772d7237e6b
Rishi1506 <rishitharajput15@gmail.com>
2023-05-11 14:06:27 +0530

    Done

---
+++ CRUD/CrudDemo/CrudDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,88 @@
+﻿using CrudDemo.Models;
+using CrudDemo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        IEmployeeService _employeeService;
+        private EmployeeController(IEmployeeService service)
+        {
+            _employeeService = service;
+        }
+
+        //get all employee
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetAllEmployees()
+        {
+            try
+            {
+                var employees = _employeeService.GetEmployeesList();
+                if (employees == null) return NotFound();
+                return Ok(employees);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        //get employee by id
+        [Route("[action]/id")]
+        public IActionResult GetEmployeesById(int id)
+        {
+            try
+            {
+                var employees = _employeeService.GetEmployeeDetailsById(id);
+                if (employees == null) return NotFound();
+                return Ok(employees);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        //save employee details
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult SaveEmployees(Employees employeeModel)
+        {
+            try
+            {
+                var model = _employeeService.SaveEmployee(employeeModel);
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+
+        //delete employee
+        [Route("[action]")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            try
+            {
+                var model = _employeeService.DeleteEmployee(id);
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
---
+++ CRUD/CrudDemo/CrudDemo/Services/IEmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using CrudDemo.Models;
+using CrudDemo.VeiwModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudDemo.Services
+{
+    interface IEmployeeService
+    {
+    
+        /// get list of all employees
+       
+        List<Employees> GetEmployeesList();
+
+   
+        /// get employee details by employee id
+        /// <param name="empId"></param>
+     
+        Employees GetEmployeeDetailsById(int empId);
+
+        ///  add edit employee
+        /// <param name="employeeModel"></param>
+    
+        ResponseModel SaveEmployee(Employees employeeModel);
+
+
+       
+        /// delete employee
+        /// <param name="employeeId"></param>
+      
+        ResponseModel DeleteEmployee(int employeeId);
+    }
+}
